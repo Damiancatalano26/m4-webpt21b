a0957d61334cde63e312c05ece409c016418c16a
Damiancatalano26 <damiancatalano26@gamil.com>
2024-11-07 18:43:24 -0300

    ultimos cambios

--- service/orders.ts
+++ service/orders.ts
@@ -1,8 +1,11 @@
 import { Cart } from "@/contexts/cartContext"
-const apiUrl = process.env.API_URL;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL
 
 export const postOrders = async (userId: number,token:string, cart:Cart[]) => {
     const data = {userId, products:cart.map((item) => item.id)}
+    console.log(process.env)
+    console.log("API URL:", apiUrl); // Debe mostrar la URL completa, como https://5666-190-120-246-4.ngrok-free.app
+
     const res = await fetch(`${apiUrl}/orders`, {
         method: "POST",
         body: JSON.stringify(data),
